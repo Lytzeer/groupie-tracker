676cf88c2b8964a17b44252cc79c65cad3a01200
Timothee DURAN <timothee.duran@ynov.com>
2023-01-17 16:18:50 +0100

    filter datas

--- server/main.go
+++ server/main.go
@@ -20,6 +20,7 @@ func main() {
 	donnermoi = gp.SetData(Da, Ar, Gl, Re)
 	fmt.Println("Starting server on port 8080")
 	http.HandleFunc("/", HandleIndex)
+	http.HandleFunc("/filter", HandleFilter)
 	http.HandleFunc("/infos", HandleInfos)
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
@@ -33,6 +34,43 @@ func HandleIndex(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, donnermoi)
 	return
 }
+
+func HandleFilter(w http.ResponseWriter, r *http.Request) {
+	buttons := r.FormValue("Member")
+	creation := r.FormValue("creationdate")
+	album := r.FormValue("albumdate")
+	city := r.FormValue("city")
+	fmt.Println(buttons)
+	fmt.Println(creation)
+	fmt.Println(album)
+	fmt.Println(city)
+	// Iid, _ := strconv.Atoi(id)
+
+	// Iid = Iid - 1
+	// loc := donnermoi.Location[Iid]
+	// art := donnermoi.Artist[Iid]
+	// dat := donnermoi.Date[Iid]
+	// a := donnermoi.Locs[Iid]
+	// donnerartist := gpd.ArtistInfos{}
+	// donnerartist.Artist = art
+	// donnerartist.Location = loc
+	// donnerartist.All = a
+
+	// for i := 0; i < len(dat.Dates); i++ {
+	// 	if string(dat.Dates[i][0]) == "*" {
+	// 		dat.Dates[i] = dat.Dates[i][1:]
+	// 	}
+	// }
+
+	// donnerartist.Date = dat
+
+	var tmpl *template.Template
+	tmpl = template.Must(template.ParseFiles("./static/artistes.html"))
+	tmpl.Execute(w, donnermoi)
+	http.Redirect(w, r, "/", 302)
+	return
+}
+
 func HandleInfos(w http.ResponseWriter, r *http.Request) {
 	id := r.FormValue("id")
 
