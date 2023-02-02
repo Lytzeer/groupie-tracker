0717dfe19154ce5abdfbfc3b53bdb99bbb7db551
Mathéo Lang <matheo.lang@ynov.com>
2023-02-02 16:21:38 +0100

    geolocalization

--- server/main.go
+++ server/main.go
@@ -227,7 +227,7 @@ func HandleInfos(w http.ResponseWriter, r *http.Request) {
 		cartes = append(cartes, carte)
 	}
 
-	donnerartist.Cartes = cartes
+	donnerartist.Carte = carte
 	var tmpl *template.Template
 	tmpl = template.Must(template.ParseFiles("./static/info.html"))
 	tmpl.Execute(w, donnerartist)
